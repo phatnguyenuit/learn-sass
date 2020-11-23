944e137968afe32d9c4e769970ae1abef87da1e7
Nguyen Phat <phatnt.uit@gmail.com>
2020-11-23 20:25:06 +0700

    Configure Gulp, SASS and Autoprefixer

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,15 +1,43 @@
 const gulp = require('gulp');
-const autoprefixer = require('autoprefixer');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+const prettier = require('gulp-prettier');
 const sourcemaps = require('gulp-sourcemaps');
-const postcss = require('gulp-sourcemaps');
 
-// https://medium.com/@jhinter/setting-up-gulp-to-compile-scss-in-less-than-5-minutes-fee8bea2b68b
-
-gulp.task('autoprefixer', () => {
-  return gulp
-    .src('./styles/*.scss')
-    .pipe(sourcemaps.init())
-    .pipe(postcss([autoprefixer()]))
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('./out'));
+const mode = require('gulp-mode')({
+  modes: ["production", "development"],
+  default: "development",
+  verbose: false
 });
+
+const input = 'styles/**/*.scss';
+const output = 'dist';
+
+const compile_scss_to_css = () => {
+  return gulp //add gulp without return to keep session going
+    // Find all .scss files from the sass/ folder
+    .src(input)
+    // Init sourcemaps if on development mode
+    .pipe(mode.development(sourcemaps.init()))
+    // Run Sass on those files || pipe adds everything together
+    .pipe(sass().on('error', sass.logError)) //error log to keep session going when scss contains error
+    // Run autoprefixer
+    .pipe(autoprefixer())
+    // Run prettier if on development mode
+    .pipe(mode.development(prettier()))
+    // Run sourcemaps if on development mode
+    .pipe(mode.development(sourcemaps.write('.')))
+    // Minify css if on production mode
+    .pipe(mode.production(cleanCSS()))
+    // Write the resulting CSS in the output folder
+    .pipe(gulp.dest(output));
+};
+
+
+gulp.task('watch', function () {
+  gulp.watch(input, compile_scss_to_css);
+});
+
+gulp.task('build', compile_scss_to_css);
+gulp.task('default', compile_scss_to_css);
--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # learn-sass
 
- Learning SASS/SCSS from scratch 
+Learning SASS/SCSS from scratch 
+
+TODO
+
+[x] Configure Gulp and Autoprefixer for SASS.
+
+[ ] Config Autoprefixer to apply browserlist on [package.json](./package.json)
